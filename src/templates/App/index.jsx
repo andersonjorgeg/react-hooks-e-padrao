4003b5d5ec4f924f1bd1983d35394262579d5c07
Anderson Jorge <anderson.jorge33@gmail.com>
2021-07-29 21:15:04 -0300

    Organização com useEffect, useReducer, useContext e useRef

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import { PostsProvider } from '../../contexts/PostsProvider';
+import { Posts } from '../../components/Posts';
+
+function App() {
+  return (
+    <PostsProvider>
+      <div>
+        <Posts />
+      </div>
+    </PostsProvider>
+  );
+}
+export default App;
