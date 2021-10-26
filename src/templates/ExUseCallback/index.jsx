4e425861348515b9f7ca7e09ee60e00cd7002c5e
Anderson Jorge <anderson.jorge33@gmail.com>
2021-10-25 21:18:47 -0300

    ExUseCallback

--- src/templates/ExUseCallback/index.jsx
+++ src/templates/ExUseCallback/index.jsx
@@ -1,9 +1,39 @@
+import P from 'prop-types';
 import './styles.css';
+import React, { useState, useCallback } from 'react';
+
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('filho renderizou');
+
+  return (
+    <button
+      onClick={() => {
+        incrementButton(10);
+      }}
+    >
+      +
+    </button>
+  );
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
 
 export default function ExUseCallback() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
+  }, []);
+
+  console.log('pai renderizou');
+
   return (
     <div className="App">
-      <h1>Exemplo do Hook useCallback</h1>
+      <p>Teste 3</p>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
