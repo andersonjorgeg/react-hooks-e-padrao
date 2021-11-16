aba72dd6b0b647d8b87700870a73a77dcb7dd7a0
Anderson Jorge <anderson.jorge33@gmail.com>
2021-11-15 21:03:25 -0300

    Criando meu segundo hook - Parte 2

--- src/templates/ExUseMySecondHook/index.jsx
+++ src/templates/ExUseMySecondHook/index.jsx
@@ -1,55 +1,146 @@
 import './styles.css';
+//importando o hook
+import { useEffect, useState, useRef } from 'react';
 
-import { useEffect, useState } from 'react';
+// função para verificar se dois objetos são iguais
+const isObjectEqual = (objA, objB) => {
+  //transforma os objetos em strings e verifica se são iguais
+  return JSON.stringify(objA) === JSON.stringify(objB);
+};
 
+// criando o hook useFetch para fazer requisições
 const useFetch = (url, options) => {
-  // seta o estado inicial
+  // cria o estado inicial do result
   const [result, setResult] = useState(null);
+  // cria o estado inicial do loading
   const [loading, setLoading] = useState(false);
+  // criando estado de carregamento da página
+  const [shouldLoading, setShouldLoading] = useState(false);
+  // criando a referência para url
+  const urlRef = useRef(url);
+  // criando a referência para options
+  const optionsRef = useRef(options);
 
-  // executa a requisição
+  // usando o hook useEffect para monitorar a url e options
   useEffect(() => {
+    // flag para seta setShouldLoading
+    let changed = false;
+
+    // atualizando a referência da url
+    if (!isObjectEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      // seta o flag para true
+      changed = true;
+    }
+    // atualizando a referência da options
+    if (!isObjectEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      // seta o flag para true
+      changed = true;
+    }
+    if (changed) {
+      setShouldLoading((s) => !s);
+    }
+  }, [url, options]);
+
+  // usando o hook useEffect para chamar o hook useFetch
+  useEffect(() => {
+    // flag para seta o wait como false
+    let wait = false;
+
     console.log('EFFECT', new Date().toLocaleString());
-    // seta o loading
+    console.log(optionsRef.current.headers);
+    // seta o state do loading
     setLoading(true);
 
-    // faz a requisição
+    // cria a função que faz a requisição
     const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 3000));
+      // cria uma promise para esperar a resposta da requisição
+      await new Promise((r) => setTimeout(r, 1000));
       try {
-        const response = await fetch(url, options);
+        // cria uma constante para pegar a resposta da requisição
+        const response = await fetch(urlRef.current, optionsRef);
+        // cria uma constante para pegar a resposta transformada em json
         const jsonResult = await response.json();
-        setResult(jsonResult);
-        setLoading(false);
+
+        // se wait for true
+        if (!wait) {
+          // seta o resultado no estado
+          setResult(jsonResult);
+          // seta o loading como false
+          setLoading(false);
+        }
+        // caso ocorra algum erro
       } catch (error) {
-        setLoading(false);
+        // se o wait for true
+        if (!wait) {
+          // seta o loading como false
+          setLoading(false);
+        }
         throw error;
       }
     };
+    // chama a função fetchData
     fetchData();
-  }, [url, options]);
 
+    // limpeza do hook useEffect
+    return () => {
+      // seta o wait como true
+      wait = true;
+    };
+    // array de dependências do useEffect
+  }, [shouldLoading]);
+
+  // retorna o resultado e o loading da função useFetch
   return [result, loading];
 };
 
 export const ExUseMySecondHook = () => {
+  const [postId, setPostId] = useState('');
+  // usando o hook useFetch
   const [result, loading] = useFetch(
-    'https://jsonplaceholder.typicode.com/posts',
-    /*  {
-      method: 'GET',
+    'https://jsonplaceholder.typicode.com/posts/' + postId,
+    {
       headers: {
-        ABC: '1',
+        abc: '200' + postId,
       },
     },
-  ); */
   );
 
+  // usando o hook useEffect para monitorar o postId
+  useEffect(() => {
+    console.log(`ID do post: ${postId}`);
+  }, [postId]);
+
+  // condicional para mostrar o loading
   if (loading) {
     return <div>Carregando...</div>;
   }
 
+  const handleClick = (id) => {
+    setPostId(id);
+  };
+
+  // condicional para mostrar o resultado
   if (!loading && result) {
-    console.log(result);
+    return (
+      <div className="App">
+        {result?.length > 0 ? (
+          result.map((post) => (
+            <div key={`post-${post.id}`} onClick={() => handleClick(post.id)}>
+              <p>
+                post {post.id} - {post.title}
+              </p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>post - {result.id}</p>
+            <p>{result.body}</p>
+          </div>
+        )}
+      </div>
+    );
   }
 
   return (
