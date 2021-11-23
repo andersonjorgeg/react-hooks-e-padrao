2721a8ad1c60af8dba06272f48f01c23500b2df3
Anderson Jorge <anderson.jorge33@gmail.com>
2021-11-23 19:07:42 -0300

    Ordem dos hooks

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { ExUseMediaQuery } from './templates/ExUseMediaQuery';
+import { OrdemHooks } from './templates/OrdemHooks';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ExUseMediaQuery />
+    <OrdemHooks />
   </React.StrictMode>,
   document.getElementById('root'),
 );
